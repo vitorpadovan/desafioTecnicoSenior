b214015396c101d3a0a459cb1d38852dc18d8e41
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2025-04-05 10:47:07 -0300

    Adicionando tratativa de erro na inicialização do banco.

--- src/Backend/Bff/Extensions/DatabaseInitializationExtension.cs
+++ src/Backend/Bff/Extensions/DatabaseInitializationExtension.cs
@@ -9,55 +9,68 @@ namespace Bff.Extensions
     {
         public static void InitDatabase(this IServiceProvider serviceProvider)
         {
-            using (var scope = serviceProvider.CreateScope())
+            var rentryNull = Policy
+                .Handle<NullReferenceException>()
+                .WaitAndRetry(
+                    retryCount: 5,
+                    sleepDurationProvider: attempt => TimeSpan.FromSeconds(attempt * 3),
+                    onRetry: (exception, timeSpan, attempt, context) =>
+                    {
+                        var logger = serviceProvider.GetRequiredService<ILogger<AppDbContext>>();
+                        logger.LogWarning("Tentativa {Attempt} falhou devido a NullReferenceException: {Message}", attempt, exception.Message);
+                    });
+            rentryNull.Execute(() =>
             {
-                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                var logger = scope.ServiceProvider.GetRequiredService<ILogger<AppDbContext>>();
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<AppDbContext>>();
 
-                var retryPolicy = Policy
-                    .Handle<NpgsqlException>(ex => ex.SqlState.StartsWith("08"))
-                    .Or<TimeoutException>()
-                    .Or<NullReferenceException>()
-                    .WaitAndRetry(
-                        retryCount: 5,
-                        sleepDurationProvider: attempt => TimeSpan.FromSeconds(attempt * 3),
-                        onRetry: (exception, timeSpan, attempt, context) =>
+                    var retryPolicy = Policy
+                        .Handle<NpgsqlException>(ex => ex.SqlState.StartsWith("08"))
+                        .Or<TimeoutException>()
+                        .WaitAndRetry(
+                            retryCount: 5,
+                            sleepDurationProvider: attempt => TimeSpan.FromSeconds(attempt * 3),
+                            onRetry: (exception, timeSpan, attempt, context) =>
+                            {
+                                logger.LogWarning("Tentativa {Attempt} falhou devido a problema de conexão: {Message}", attempt, exception.Message);
+                            });
+
+                    try
+                    {
+                        retryPolicy.Execute(() =>
                         {
-                            logger.LogWarning("Tentativa {Attempt} falhou devido a problema de conexão: {Message}", attempt, exception.Message);
+                            dbContext.Database.EnsureCreated();
                         });
-
-                try
-                {
-                    retryPolicy.Execute(() =>
+                    }
+                    catch (NpgsqlException ex) when (!ex.SqlState.StartsWith("08"))
                     {
-                        dbContext.Database.EnsureCreated();
-                    });
-                }
-                catch (NpgsqlException ex) when (!ex.SqlState.StartsWith("08"))
-                {
-                    //Nada a fazer
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "Database creation failed após múltiplas tentativas com o tipo de erro {erro}", ex.GetType().Name);
-                }
-
-                try
-                {
-                    retryPolicy.Execute(() =>
+                        //Nada a fazer
+                    }
+                    catch (Exception ex)
                     {
-                        dbContext.Database.Migrate();
-                    });
+                        logger.LogError(ex, "Database creation failed após múltiplas tentativas com o tipo de erro {erro}", ex.GetType().Name);
+                    }
+
+                    try
+                    {
+                        retryPolicy.Execute(() =>
+                        {
+                            dbContext.Database.Migrate();
+                        });
+                    }
+                    catch (NpgsqlException ex) when (!ex.SqlState.StartsWith("08"))
+                    {
+                        //Nada a fazer
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "Database migration failed após múltiplas tentativas com o tipo de erro {erro}", ex.GetType().Name);
+                    }
                 }
-                catch (NpgsqlException ex) when (!ex.SqlState.StartsWith("08"))
-                {
-                    //Nada a fazer
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "Database migration failed após múltiplas tentativas com o tipo de erro {erro}", ex.GetType().Name);
-                }
-            }
+            });
+
         }
     }
 }
